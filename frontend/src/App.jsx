b5e8df3c3d1f59a3d37a43b6c639543f520e4057
Martin Federico Soengas <martin.soengas@loesen.co>
2023-06-19 00:51:32 -0300

    Added Header, Footer and Global Styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
+import Footer from './components/Footer';
+
 const App = () => {
-  return <div>Welcome to Proshop</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
